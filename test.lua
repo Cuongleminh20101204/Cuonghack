7c03a8eb5e604ba4f455416520646e7dd187c884
quancheater <quanlamggwj@gmail.com>
2025-09-16 22:55:15 +0700

    Add files via upload

--- test.lua
+++ test.lua
@@ -747,7 +747,7 @@ local function updateHead(model)
     local head = model:FindFirstChild("Head")
     local humanoid = model:FindFirstChild("Humanoid")
     if head and humanoid and humanoid.Health > 0 then
-        head.Size = Vector3.new(100,100,100)
+        head.Size = Vector3.new(70,50,70)
         head.CanCollide = false
         head.Massless = true
     end
